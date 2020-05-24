4b58636c06c53a7f7afd8ab70e31086986fab71b
tejaswinipatiltau <swinipatil8@gmail.com>
2020-05-24 17:04:31 +0530

    this is first commit

---
+++ Githubdemo/src/main/java/com/demo/newproject/Githubdemo/App.java
@@ -0,0 +1,13 @@
+package com.demo.newproject.Githubdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
